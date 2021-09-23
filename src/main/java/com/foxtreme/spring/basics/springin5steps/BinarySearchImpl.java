e910ab4a2d15a96218811876c6c3a6844c7b6146
ccharry <chriseth@gmail.com>
2021-09-22 22:49:00 -0500

    types of autowiring

--- src/main/java/com/foxtreme/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/foxtreme/spring/basics/springin5steps/BinarySearchImpl.java
@@ -1,23 +1,22 @@
 package com.foxtreme.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 
 	@Autowired
-	private SortAlgorithm sortAlgorithm;
-	
-	public BinarySearchImpl(SortAlgorithm sortAlgorithm) {
-		super();
-		this.sortAlgorithm = sortAlgorithm;
-	}
+	private SortAlgorithm quickSortAlgorithm;
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
-		int [] sortedNumbers = this.sortAlgorithm.sort(numbers);
-		System.out.println(sortAlgorithm);
+		int [] sortedNumbers = this.quickSortAlgorithm.sort(numbers);
+		System.out.println(quickSortAlgorithm);
 		return 3;
 	}
 	
