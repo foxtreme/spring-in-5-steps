18aee2920a116ff21f615e66ee95cb5ace38f94e
ccharry <chriseth@gmail.com>
2021-09-25 00:06:40 -0500

    spring component annotations

--- src/main/java/com/foxtreme/spring/basics/springin5steps/basic/BinarySearchImpl.java
+++ src/main/java/com/foxtreme/spring/basics/springin5steps/basic/BinarySearchImpl.java
@@ -3,9 +3,9 @@ package com.foxtreme.spring.basics.springin5steps.basic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 //@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
@@ -15,7 +15,7 @@ public class BinarySearchImpl {
 
 	public int binarySearch(int[] numbers, int numberToSearchFor) {
 		
-		int [] sortedNumbers = this.quickSortAlgorithm.sort(numbers);
+		this.quickSortAlgorithm.sort(numbers);
 		System.out.println(quickSortAlgorithm);
 		return 3;
 	}
--- src/main/java/com/foxtreme/spring/basics/springin5steps/basic/BubbleSortAlgorithm.java
+++ src/main/java/com/foxtreme/spring/basics/springin5steps/basic/BubbleSortAlgorithm.java
@@ -1,8 +1,8 @@
 package com.foxtreme.spring.basics.springin5steps.basic;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class BubbleSortAlgorithm implements SortAlgorithm{
 	
 	public int [] sort(int[] numbers) {
